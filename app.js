456becdf34f82e83e980c109d4a7f8b91c9167db
RazorZ7X <elian_farias2000@outlook.com>
2025-08-11 14:20:58 -0400

    Agrega función que permita al usuario ingresar un nombre en el campo de texto y añadirlo a la lista de amigos creada anteriormente con su validación respectiva

--- app.js
+++ app.js
@@ -2,3 +2,19 @@
 
 //Variable de tipo array, que almacenará los nombres de los amigos ingresados
 let amigos = [];
+
+//Función que permite al usuario ingresar un nombre en el campo de texto y añadirlo a la lista de amigos creada anteriormente
+function agregarAmigo() {
+    //Obtener el valor del input
+    let input = document.getElementById("amigo").value;
+    //Validar que el campo no esté vacío
+    if (input === "") {
+        alert("Por favor, ingresa un nombre.");
+    } else {
+        //Agregar el nombre al array de amigos
+        amigos.push(input);
+    }
+    //Limpiar el campo de texto
+    document.getElementById("amigo").value = "";
+}
+
